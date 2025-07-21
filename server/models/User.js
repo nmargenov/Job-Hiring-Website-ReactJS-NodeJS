51856e5967573bbb1891c022be5cbbc3fc92901f
nmargenov <nikolay_margenov@yahoo.com>
2025-07-21 21:49:13 +0300

    update user scheme

--- server/models/User.js
+++ server/models/User.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
-const { emailRegex } = require("../utils/regex");
+const { emailRegex, phoneRegex } = require("../utils/regex");
 
 const userSchema = new mongoose.Schema({
     email:{
         type: String,
         required: [true, 'Email is required!'],
-        minLength: [3, 'Email must be between 3 and 50 characters!'],
-        maxLength: [50, 'Email must be between 3 and 50 characters!'],
+        minLength: [3, 'Email must be between 3 and 50 characters'],
+        maxLength: [50, 'Email must be between 3 and 50 characters'],
         validate: {
             validator: (value) => emailRegex.test(value),
             message: "Invalid email format!",
@@ -15,26 +15,47 @@ const userSchema = new mongoose.Schema({
     },
     role: {
         type: String,
+        required:[true, "Role is required"],
         enum: ["seeker","hirer","admin"],
         default: 'seeker'
     },
     phone: {
         type: String,
         default: '',
+        required: [true, "Phone number is required"],
+        validate: {
+        validator: function (val) {
+            return !this.isSetup || phoneRegex.test(val);
+        },
+        message: 'Phone must start with +<countrycode> or 0 and contain at least 6 digits'
+    }
     },
     firstName: {
         type: String,
         default: '',
-        maxLength: [30, 'First name must be up to 30 characters!'],
+        required: [true, "First name is required"],
+        validate: {
+        validator: function (val) {
+            return !this.isSetup || (val.length >= 5 && val.length <=30);
+        },
+        message: 'First name must be between 5 and 30 characters'
+    }
     },
     lastName: {
         type: String,
         default: '',
-        maxLength: [30, 'Last name must be up to 30 characters!'],
+        required: [true, "Last name is required"],
+        validate: {
+        validator: function (val) {
+            return !this.isSetup || (val.length >= 5 && val.length <=30);
+        },
+        message: 'Last name must be between 5 and 30 characters'
+    }   
     },
-        isSetup: {
+    isSetup: {
         type: Boolean,
         default: false,
+        required: [true, "isSetup is required"]   
     }
 }, { timestamps: true });
 
