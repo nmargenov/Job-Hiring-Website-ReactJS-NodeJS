41125028a0c86a1704ecbd32dba4d8bf319e33e9
nmargenov <nikolay_margenov@yahoo.com>
2025-07-22 11:16:08 +0300

    Added validations for logincode model

--- server/models/LoginCode.js
+++ server/models/LoginCode.js
@@ -1,8 +1,20 @@
 const mongoose = require("mongoose");
 
 const LoginCodeSchema = new mongoose.Schema({
-    email: String,
-    code: String,
+    email:{
+           type: String,
+           required: [true, 'Email is required'],
+           minLength: [3, 'Email must be between 3 and 50 characters'],
+           maxLength: [50, 'Email must be between 3 and 50 characters'],
+           validate: {
+               validator: (value) => emailRegex.test(value),
+               message: "Invalid email format!",
+           }
+       },
+    code: {
+        type: String,
+        required: [true, 'Code is required'],
+    },
     expiresAt: Date,
 }, { timestamps: true });
 
