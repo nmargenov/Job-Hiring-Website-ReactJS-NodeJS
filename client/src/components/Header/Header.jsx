79332cca6b346d077a33023b0dfd6848ffac3216
nmargenov <nikolay_margenov@yahoo.com>
2025-07-28 22:27:55 +0300

    Added mulit-language support (en and bg)

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react';
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faList, faClose, faHome, faContactCard, faPhone } from "@fortawesome/free-solid-svg-icons"
+import { faList, faClose, faHome, faPhone } from "@fortawesome/free-solid-svg-icons"
 import { NavItem } from './navItem/NavItem';
+import { useTranslation } from 'react-i18next';
+
 
 import styles from './header.module.css';
 
 export const Header = () => {
     const [isOpen, setIsOpen] = useState(false);
+
+    const { t } = useTranslation();
 
     function toggleOpen() {
         setIsOpen(!isOpen);
@@ -18,6 +22,14 @@ export const Header = () => {
             toggleOpen();
         }
     }
+
+    const { i18n } = useTranslation();
+
+    const switchLang = (lng) => {
+        i18n.changeLanguage(lng);
+    };
+
+
     return (
         <header className={styles['header']}>
             <div className={styles['logo']}>
@@ -26,7 +38,7 @@ export const Header = () => {
             </div>
 
             {!isOpen && <div className={styles['right-div']}>
-                <Link to={"/login"} className={styles['login-button']}>Вход</Link>
+                <Link to={"/login"} className={styles['login-button']}>{t("login")}</Link>
                 <FontAwesomeIcon tabIndex={0}
                     onClick={toggleOpen} onKeyDown={(e) => { onKey(e) }} icon={isOpen ? faClose : faList} />
             </div>}
@@ -36,11 +48,15 @@ export const Header = () => {
                     <div onClick={toggleOpen} className={styles['nav-div']}></div>
                     <nav className={styles['nav']}>
                         <div className={styles['section']}>
-                            <Link onClick={toggleOpen} to={"/login"} className={styles['login-button']}>Вход</Link>
+                            <Link onClick={toggleOpen} to={"/login"} className={styles['login-button']}>{t("login")}</Link>
                         </div>
                         <div className={styles['section']}>
-                            <NavItem destination={'/'} icon={faHome} text={"Начало"} toggleOpen={toggleOpen}/>
-                            <NavItem destination={'/contacts'} icon={faPhone} text={"Контакти"} toggleOpen={toggleOpen}/>
+                            <NavItem destination={'/'} icon={faHome} text={t("homepage")} toggleOpen={toggleOpen} />
+                            <NavItem destination={'/contacts'} icon={faPhone} text={t("contacts")} toggleOpen={toggleOpen} />
+                        </div>
+                        <div className={styles['section']}>
+                            <button onClick={() => switchLang('en')}>English</button>
+                            <button onClick={() => switchLang('bg')}>Български</button>
                         </div>
                     </nav>
                 </>
