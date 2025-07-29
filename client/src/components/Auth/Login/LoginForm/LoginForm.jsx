6d1fa84e388f280ca351b2559aef67a26f8ec33b
nmargenov <nikolay_margenov@yahoo.com>
2025-07-29 23:53:44 +0300

    custom hook for forms

--- client/src/components/Auth/Login/LoginForm/LoginForm.jsx
+++ client/src/components/Auth/Login/LoginForm/LoginForm.jsx
@@ -1,10 +1,28 @@
 import { t } from 'i18next';
 import styles from './loginForm.module.css';
+import { useForm } from '../../../../hooks/useForm';
+import { useEffect } from 'react';
 
 export const LoginForm = () => {
 
+    const initialValues = {
+        email: ''
+    }
+
+    const { values, onInputChange, onSubmitHandler, isLoading, setIsLoading, errorMsg, setErrorMsg } = useForm(initialValues);
+
     function onSubmit(e) {
-        e.preventDefault();
+        onSubmitHandler(e);
+        setIsLoading(true);
+        console.log(values);
+        // useEffect(() => {
+        //     const timer = setTimeout(() => {
+        //         console.log(values);
+        //         setIsLoading(false);
+        //     }, 2000);
+
+        //     return () => clearTimeout(timer); // cleanup on unmount
+        // }, []);
     }
 
     return (
@@ -18,15 +36,15 @@ export const LoginForm = () => {
                     required
                     minLength="3"
                     maxLength="40"
-                // className={`${values.username.length > 0 && values.username.length < 3 || !isValidUsername(values.username) ? styles.invalidField : ""}`}
-                // value={values.username}
-                // onChange={onInputChange}
-                // disabled={isLoading}
+                    // className={`${values.username.length > 0 && values.username.length < 3 || !isValidUsername(values.username) ? styles.invalidField : ""}`}
+                    value={values.email}
+                    onChange={onInputChange}
+                    disabled={isLoading}
                 />
                 <label htmlFor="email">{t('email')}</label>
             </div>
             <div className={styles['submit-div']}>
-                <input type="submit" value={t('login')}/>
+                <input type="submit" value={t('login')} disabled={isLoading}/>
             </div>
         </form>
     )
