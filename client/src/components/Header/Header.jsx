2549e5e2648a3a9a46594c3f6aec730343b49422
nmargenov <nikolay_margenov@yahoo.com>
2025-07-29 12:58:28 +0300

    cookie settings in navbar

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react';
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faList, faClose, faHome, faPhone } from "@fortawesome/free-solid-svg-icons"
+import { faList, faClose, faHome, faPhone, faCookie } from "@fortawesome/free-solid-svg-icons"
 import { NavItem } from './navItem/NavItem';
 import { useTranslation } from 'react-i18next';
-
+import { useConsent } from '../../contexts/CookieConsentContext';
 
 import styles from './header.module.css';
 
+
 export const Header = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const { setShowModal } = useConsent();
 
     const { t } = useTranslation();
 
@@ -35,6 +37,9 @@ export const Header = () => {
         }
     }
 
+    function onCookieClick(){
+        setShowModal(true);
+    }
 
     return (
         <header className={styles['header']}>
@@ -57,8 +62,9 @@ export const Header = () => {
                             <Link onClick={toggleOpen} to={"/login"} className={styles['login-button']}>{t("login")}</Link>
                         </div>
                         <div className={styles['section']}>
-                            <NavItem destination={'/'} icon={faHome} text={t("homepage")} toggleOpen={toggleOpen} />
-                            <NavItem destination={'/contacts'} icon={faPhone} text={t("contacts")} toggleOpen={toggleOpen} />
+                            <NavItem destination={'/'} icon={faHome} text={t("homepage")} onClick={toggleOpen} />
+                            <NavItem destination={'/contacts'} icon={faPhone} text={t("contacts")} onClick={toggleOpen} />
+                            <NavItem onClick={onCookieClick} icon={faCookie} text={t("cookie-title")} />
                         </div>
                         <div className={styles['section']}>
                             <a onKeyDown={(e)=>{onLanguageClick(e,"en")}} tabIndex={0} className={styles['language-option']} onClick={() => switchLang('en')}>English</a>
--- client/src/components/Header/navItem/NavItem.jsx
+++ client/src/components/Header/navItem/NavItem.jsx
@@ -2,14 +2,14 @@ import styles from './navItem.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Link } from "react-router-dom";
 
-export const NavItem = ({ icon, text, toggleOpen, destination }) => {
+export const NavItem = ({ icon, text, onClick, destination }) => {
     function onKey(e) {
         if (e.key === 'Enter') {
             toggleOpen();
         }
     }
     return (
-        <Link onClick={toggleOpen} to={destination} tabIndex={0} onKeyDown={(e) => { onKey(e) }} className={styles['item']}>
+        <Link onClick={onClick} to={destination} tabIndex={0} onKeyDown={(e) => { onKey(e) }} className={styles['item']}>
             <FontAwesomeIcon icon={icon} />
             <span>{text}</span>
         </Link>
