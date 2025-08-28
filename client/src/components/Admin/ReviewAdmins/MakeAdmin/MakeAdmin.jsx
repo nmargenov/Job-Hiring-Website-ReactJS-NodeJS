dacd32c6d5265cd52545e50f283fb8290ec22bb9
nmargenov <nikolay_margenov@yahoo.com>
2025-08-28 13:56:05 +0300

    frontend search users by email to make admin

--- client/src/components/Admin/ReviewAdmins/MakeAdmin/MakeAdmin.jsx
+++ client/src/components/Admin/ReviewAdmins/MakeAdmin/MakeAdmin.jsx
@@ -1,5 +1,44 @@
-export const MakeAdmin = () => { 
-    return(
-        <h1>make admin</h1>
+import { useTranslation } from 'react-i18next';
+import { useForm } from '../../../../hooks/useForm';
+import { Form } from '../../../shared/Form/Form';
+import { FormInput } from '../../../shared/FormInput/FormInput';
+import styles from './makeAdmin.module.css';
+
+export const MakeAdmin = () => {
+    const initialValues = {
+        email: ''
+    }
+
+    const { t } = useTranslation();
+
+    const { values, onInputChange, onSubmitHandler, isLoading, setIsLoading, errorMsg, setErrorMsg } = useForm(initialValues);
+
+    function onSubmit(e) {
+        onSubmitHandler(e);
+        console.log("test")
+    }
+
+    return (
+        <div className={styles['main-div']}>
+            <div className={styles['email-div']}>
+                <Form onSubmit={onSubmit}
+                    errorMsg={errorMsg}
+                    buttons={
+                        <input type="submit" value={t('search')} disabled={(isLoading || (values.email.length >= 0 && values.email.length < 5))} />
+                    }
+                >
+                    <FormInput
+                        formName={'email'}
+                        value={values.email}
+                        onInputChange={onInputChange}
+                        isLoading={isLoading}
+                        type={'text'}
+                        minLength={'3'}
+                        maxLength={'50'}
+                        validate={values.email.length > 0 && values.email.length < 5}
+                    />
+                </Form>
+            </div>
+        </div>
     )
 }
