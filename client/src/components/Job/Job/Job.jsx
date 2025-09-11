245fb11f6cdf86af381548fe34e57e4bb59139ee
nmargenov <nikolay_margenov@yahoo.com>
2025-09-11 12:00:27 +0300

    Page loader at fetching job

--- client/src/components/Job/Job/Job.jsx
+++ client/src/components/Job/Job/Job.jsx
@@ -3,6 +3,7 @@ import { useEffect } from "react"
 import { useParams } from "react-router-dom"
 import { getJob } from "../../../services/jobService";
 import { Page404 } from "../../Page404/Page404";
+import { Loader } from "../../shared/Loader/Loader";
 
 export const Job = () => {
 
@@ -10,14 +11,18 @@ export const Job = () => {
 
     const [job, setJob] = useState(null);
     const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
+        setIsLoading(true);
         getJob(params.jobID)
             .then((data) => {
+                setIsLoading(false);
                 setJob(data);
                 setError(false);
                 console.log(data);
             }).catch((err) => {
+                setIsLoading(false);
                 setError(true);
                 console.log(err);
             })
@@ -25,8 +30,9 @@ export const Job = () => {
 
     return (
         <>
-            {error && <Page404 />}
-            {!error && <h1>job</h1>}
+            {isLoading && <Loader />}
+            {!isLoading && error && <Page404 />}
+            {!isLoading && !error && <h1>job</h1>}
         </>
     )
 }
