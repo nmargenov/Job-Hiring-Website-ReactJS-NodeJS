ce2ec32a75be1a7f13481037e3a7c757f47d4da2
nmargenov <nikolay_margenov@yahoo.com>
2025-09-11 11:55:20 +0300

    Page 404 on invalid jobID (not found job)

--- client/src/components/Job/Job/Job.jsx
+++ client/src/components/Job/Job/Job.jsx
@@ -2,23 +2,31 @@ import { useState } from "react";
 import { useEffect } from "react"
 import { useParams } from "react-router-dom"
 import { getJob } from "../../../services/jobService";
+import { Page404 } from "../../Page404/Page404";
 
 export const Job = () => {
 
     const params = useParams();
 
     const [job, setJob] = useState(null);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         getJob(params.jobID)
             .then((data) => {
+                setJob(data);
+                setError(false);
                 console.log(data);
             }).catch((err) => {
+                setError(true);
                 console.log(err);
             })
     }, [])
 
     return (
-        <h1>job</h1>
+        <>
+            {error && <Page404 />}
+            {!error && <h1>job</h1>}
+        </>
     )
 }
