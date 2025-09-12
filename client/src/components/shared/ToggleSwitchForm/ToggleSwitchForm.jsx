3e5574017d773ee8d0f9a008f987eb3f9c396686
nmargenov <nikolay_margenov@yahoo.com>
2025-09-12 14:47:06 +0300

    ToggleSwitch for forms

---
+++ client/src/components/shared/ToggleSwitchForm/ToggleSwitchForm.jsx
@@ -0,0 +1,32 @@
+import { useRef } from 'react';
+import { handleKeyPress } from '../../../utils/handleKeyPress'
+import styles from './toggleSwitchForm.module.css'
+
+export const ToggleSwitchForm = ({ check = false, action = () => { }, disabled = false, name }) => {
+
+    const inputRef = useRef(null);
+
+    function onKey(syntheticEvent) {
+        if (syntheticEvent.key === "Enter" || syntheticEvent.key === " ") {
+            syntheticEvent.target.checked = !syntheticEvent.target.checked;
+            action(syntheticEvent);
+        }
+    }
+
+    return (
+        <>
+            <label className={styles["switch"]}>
+                <input ref={inputRef} name={name} type="checkbox" value={check} checked={check} disabled={disabled} onChange={action} />
+                <span tabIndex={0} onKeyDown={(e) => {
+                    const syntheticEvent = {
+                        ...e,
+                        target: inputRef.current,
+                        currentTarget: inputRef.current,
+                    };
+                    onKey(syntheticEvent);
+                }}
+                    className={`${styles.slider} ${styles.round}`}></span>
+            </label>
+        </>
+    )
+}
